bc2444ca7096d5552a4c0ad352f2181758efd1d0
Matt <mattdarwin@yahoo.co.uk>
2023-03-17 12:40:38 +0000

    Update output.tf

--- infra/output.tf
+++ infra/output.tf
@@ -2,3 +2,7 @@ output "s3_bucket_arn" {
   description = "ARN of S3 bucket"
   value = "${aws_s3_bucket.b.arn}"
 }
+output "s3_bucket_name" {
+  description = "name of bucket"
+  value ="${aws_s3_bucket.b.name}"
+}
